07c1249523b4da95dc151650a89b28553bcf7dee
sando87 <tjdwn0406@gmail.com>
2022-06-08 19:10:40 +0900

    UnitGunner 작업중

---
+++ Assets/Script/Component/Motions/MotionKeepAttack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MotionKeepAttack : MotionBase
+{
+    [SerializeField] private AudioClip AttackSound = null;
+    [SerializeField] private AnimationClip ReferenceAnim = null;
+    [SerializeField] private int AnimCount = 1;
+    [Range(0, 1)][SerializeField] private float FirePoint = 0.3f; //0 ~ 1
+
+    public Action<UnitBase> EventStart { get; set; }
+    public Action<UnitBase> EventUpdate { get; set; }
+    public Action EventEnd { get; set; }
+    private UnitMob Target = null;
+
+    public override bool IsReady()
+    {
+        UnitMob[] mobs = Unit.DetectAround<UnitMob>(Unit.Property.AttackRange);
+        if (mobs == null)
+            return false;
+
+        Target = mobs[0];
+        return true;
+    }
+
+    public override void OnEnter()
+    {
+        Unit.TurnHead(Target.transform.position);
+
+        //대상의 위치에 따라 재생되는 attack 애니메이션을 다르게 해줘야 한다.
+        float deg = Unit.CalcVerticalDegree(Target.transform.position);
+        int stepDegree = 180 / AnimCount;
+        int animIndex = (int)deg / stepDegree;
+        Unit.Anim.SetTrigger("attack" + (animIndex + 1));
+
+        SetAnimSpeed();
+
+        EventStart?.Invoke(Target);
+
+        RGame.Get<RSoundManager>().PlaySFX(AttackSound);
+    }
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+
+        if (Target == null || Target.CurrentState == UnitState.Death)
+        {
+            Unit.FSM.ChangeState(UnitState.Idle);
+        }
+        else if ((Target.transform.position - transform.position).magnitude > Unit.Property.AttackRange)
+        {
+            Unit.FSM.ChangeState(UnitState.Idle);
+        }
+        else
+        {
+            EventUpdate?.Invoke(Target);
+        }
+    }
+    public override void OnLeave()
+    {
+        base.OnLeave();
+        EventEnd?.Invoke();
+    }
+
+    private void SetAnimSpeed()
+    {
+        float timePerAttack = 1 / Unit.Property.AttackSpeed;
+        if (timePerAttack < ReferenceAnim.length)
+            Unit.Anim.SetFloat("attackSpeed", ReferenceAnim.length / timePerAttack);
+        else
+            Unit.Anim.SetFloat("attackSpeed", 1);
+    }
+}
+
---
+++ Assets/Script/GameObject/UserUnits/UnitGunner.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitGunner : UnitUser
+{
+    [SerializeField] private GameObject HitParticle = null;
+    [SerializeField] private GameObject BuffEffect = null;
+
+    public override void Init()
+    {
+        base.Init();
+        FSM.ChangeState(UnitState.Appear);
+        GetComponent<MotionSingleAttack>().EventFired = OnAttack;
+        GetComponent<MotionKeepAttack>().EventStart = OnAttackBeamStart;
+        GetComponent<MotionKeepAttack>().EventEnd = OnAttackBeamEnd;
+    }
+    public override void Release()
+    {
+        base.Release();
+    }
+
+    public override string SkillDescription
+    {
+        get
+        {
+            return "근접 거리의 적 유닛은 Beam공격으로 감속 효과";
+        }
+    }
+
+    private void OnAttack(UnitBase target)
+    {
+        UnitMob enemy = target as UnitMob;
+        enemy.GetDamaged(Property);
+        Vector3 pos = Utils.Random(enemy.Center, 0.1f);
+        GameObject obj = Instantiate(HitParticle, pos, Quaternion.identity);
+        Destroy(obj, 1.0f);
+    }
+
+    private void OnAttackBeamStart(UnitBase target)
+    {
+        StartCoroutine(CoAttackBeam(target));
+    }
+    IEnumerator CoAttackBeam(UnitBase target)
+    {
+        while (true)
+        {
+            ApplySlowDeBuff(target);
+            yield return new WaitForSeconds(Property.SkillDuration - 0.1f);
+        }
+    }
+    private void OnAttackBeamEnd()
+    {
+        StopAllCoroutines();
+    }
+    private void ApplySlowDeBuff(UnitBase target)
+    {
+        DeBuffSlow buff = target.BuffCtrl.FindBuff<DeBuffSlow>();
+        if (buff != null)
+            buff.RenewBuff(); //동일한 버프가 있을 경우에는 갱신만. => 결국 마린 여러마리가 공격해도 slow효과는 중복되지 않는 개념...
+        else
+            target.BuffCtrl.AddBuff(new DeBuffSlow(Property.SkillDuration));
+    }
+
+    class DeBuffSlow : BuffBase
+    {
+        //적 이동속도 20% 감소 디버프(duration시간만큼 지속)
+        public DeBuffSlow(float duration) { Duration = duration; }
+        public override void StartBuff(UnitBase target)
+        {
+            target.SR.color = Color.green;
+            target.BuffValues.MoveSpeed -= 20;
+        }
+        public override void EndBuff(UnitBase target)
+        {
+            target.SR.color = Color.white;
+            target.BuffValues.MoveSpeed += 20;
+        }
+    }
+
+}

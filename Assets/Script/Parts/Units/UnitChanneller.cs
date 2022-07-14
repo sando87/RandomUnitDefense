9aec806b9a6b5389893a1330e4b34be7608a90c4
sando87 <tjdwn0406@gmail.com>
2022-07-14 16:46:55 +0900

    유닛 기능 추가 구현

---
+++ Assets/Script/Parts/Units/UnitChanneller.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+// 레이저로 누적데미지를 입힌다.
+
+public class UnitChanneller : UnitBase
+{
+    [SerializeField] float _AttackSpeed = 0.5f;
+    float AttackSpeed { get { return _AttackSpeed * mBaseObj.BuffProp.AttackSpeed; } }
+    [SerializeField] float _AttackRange = 2;
+    float AttackRange { get { return _AttackRange * mBaseObj.BuffProp.AttackRange; } }
+
+    [SerializeField] private Sprite[] IntroSprites = null;
+    [SerializeField] private Sprite[] ProjSprites = null;
+    [SerializeField] private Sprite[] OutroSprites = null;
+
+    [SerializeField] float _SkillRange = 2;
+    float SkillRange { get { return _SkillRange * mBaseObj.BuffProp.SkillRange; } }
+    [SerializeField] float _SkillDamageStep = 0.2f;
+
+    private LaserAimming mLaserEffectObject = null;
+    private MotionActionSingle mAttackMotion = null;
+    private MotionActionLoop mLaserMotion = null;
+
+    void Start()
+    {
+        mBaseObj.MotionManager.SwitchMotion<MotionAppear>();
+
+        mAttackMotion = GetComponent<MotionActionSingle>();
+        mAttackMotion.EventFired = OnAttack;
+
+        mLaserMotion = GetComponent<MotionActionLoop>();
+        mLaserMotion.EventStart = OnAttackBeamStart;
+        mLaserMotion.EventEnd = OnAttackBeamEnd;
+        StartCoroutine(CoMotionSwitcher(mAttackMotion, 1 / AttackSpeed, AttackRange));
+        StartCoroutine(CoMotionSwitcher(mLaserMotion, 0, SkillRange));
+    }
+
+    private void OnAttack(int idx)
+    {
+        ShootProjectail(mAttackMotion.Target);
+    }
+    private void ShootProjectail(BaseObject target)
+    {
+        Vector3 dir = target.Body.Center - mBaseObj.Body.Center;
+        dir.z = 0;
+        SpritesAnimator.Play(mBaseObj.Body.Center, IntroSprites);
+
+        SpritesAnimator proj = SpritesAnimator.Play(mBaseObj.Body.Center, ProjSprites, true);
+        proj.transform.right = dir.normalized;
+        proj.transform.DOMove(target.Body.Center, 0.3f).OnComplete(() =>
+        {
+            SpritesAnimator.Play(proj.transform.position, OutroSprites);
+
+            target.Health.GetDamaged(mBaseObj.SpecProp.Damage, mBaseObj);
+
+            Destroy(proj.gameObject);
+        });
+    }
+
+    private void OnAttackBeamStart(BaseObject target)
+    {
+        if(mLaserEffectObject != null)
+        {
+            Destroy(mLaserEffectObject.gameObject);
+            mLaserEffectObject = null;
+        }
+        mLaserEffectObject = LaserAimming.Play(mBaseObj.Body.Center, target.gameObject);
+        StartCoroutine(CoAttackBeam(target));
+    }
+    IEnumerator CoAttackBeam(BaseObject target)
+    {
+        float damage = 1;
+        while (!IsOutOfSkillRange(target))
+        {
+            if(target.Health != null)
+            {
+                target.Health.GetDamaged(damage, mBaseObj);
+            }
+            yield return newWaitForSeconds.Cache(0.1f);
+            damage += _SkillDamageStep;
+        }
+        mBaseObj.MotionManager.SwitchMotion<MotionIdle>();
+    }
+    private bool IsOutOfSkillRange(BaseObject target)
+    {
+        return (target.transform.position - mBaseObj.transform.position).magnitude > (SkillRange * 1.2f);
+    }
+    private void OnAttackBeamEnd()
+    {
+        if (mLaserEffectObject != null)
+        {
+            Destroy(mLaserEffectObject.gameObject);
+            mLaserEffectObject = null;
+        }
+        StopAllCoroutines();
+    }
+}
---
+++ Assets/Script/Parts/Units/UnitDiplomat.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+// 스킬 사거리 20%증가
+
+public class UnitDiplomat : UnitBase
+{
+    [SerializeField] float _AttackSpeed = 0.5f;
+    float AttackSpeed { get { return _AttackSpeed * mBaseObj.BuffProp.AttackSpeed; } }
+    [SerializeField] float _AttackRange = 0.5f;
+    float AttackRange { get { return _AttackRange * mBaseObj.BuffProp.AttackRange; } }
+
+    [SerializeField] float _SkillRange = 3.0f;
+    float SkillRange { get { return _SkillRange * mBaseObj.BuffProp.SkillRange; } }
+
+    [SerializeField] SimpleMissile SimpleMissile = null;
+    [SerializeField] GameObject BuffEffect = null;
+
+    private MotionActionSingle mMotionAttack = null;
+    
+    void Start()
+    {
+        mBaseObj.MotionManager.SwitchMotion<MotionAppear>();
+
+        mMotionAttack = mBaseObj.MotionManager.FindMotion<MotionActionSingle>();
+        mMotionAttack.EventFired = OnAttack;
+        StartCoroutine(CoMotionSwitcher(mMotionAttack, 1 / AttackSpeed, AttackRange));
+        StartCoroutine(RepeatBuff());
+    }
+
+    void OnAttack(int idx)
+    {
+        BaseObject target = mMotionAttack.Target;
+        SimpleMissile missile = Instantiate(SimpleMissile, mBaseObj.Body.Center, Quaternion.identity);
+        missile.EventHit = OnHitMissile;
+        missile.Launch(target);
+    }
+    void OnHitMissile(BaseObject target)
+    {
+        target.Health.GetDamaged(mBaseObj.SpecProp.Damage, mBaseObj);
+    }
+
+    
+
+    private IEnumerator RepeatBuff()
+    {
+        while(true)
+        {
+            yield return newWaitForSeconds.Cache(0.5f);
+            Collider[] cols = mBaseObj.DetectAround(SkillRange, 1 << mBaseObj.gameObject.layer);
+            foreach (Collider col in cols)
+                ApplyBuff(col.GetBaseObject());
+        }
+    }
+
+    private void ApplyBuff(BaseObject target)
+    {
+        BuffSkillRangeUp buff = target.BuffCtrl.FindBuff<BuffSkillRangeUp>();
+        if (buff != null)
+            buff.RenewBuff(); //동일한 버프가 있을 경우에는 갱신만. => 중복 불가...
+        else
+        {
+            GameObject buffEffectObj = Instantiate(BuffEffect, target.Renderer.transform);
+            target.BuffCtrl.AddBuff(new BuffSkillRangeUp(buffEffectObj));
+        }
+    }
+
+    class BuffSkillRangeUp : BuffBase
+    {
+        //1초간 아군 공속 20% 증소
+        private GameObject BuffEffect;
+        public BuffSkillRangeUp(GameObject buffEffect) { Duration = 1; BuffEffect = buffEffect; }
+        public override void StartBuff(BaseObject target)
+        {
+            BuffEffect.SetActive(true);
+            target.BuffProp.SkillRange += 20;
+        }
+        public override void EndBuff(BaseObject target)
+        {
+            target.BuffProp.SkillRange -= 20;
+            Destroy(BuffEffect);
+        }
+    }
+
+}

10a0d149a775b3e93b134e8ae4253f5d8a04da1f
sando87 <tjdwn0406@gmail.com>
2022-12-22 11:21:49 +0900

    아군 스킬 기능 개선

--- Assets/Script/Parts/Units/UnitDiplomat.cs
+++ Assets/Script/Parts/Units/UnitDiplomat.cs
@@ -11,6 +11,7 @@ public class UnitDiplomat : UnitPlayer
     [SerializeField] float _AttackSpeed = 0.5f;
     [SerializeField] float _AttackRange = 0.5f;
     [SerializeField] int _PassPercent = 10;
+    [SerializeField] int _ProjectileCount = 1;
     
     [SerializeField] BuffBase BuffEffect = null;
     
@@ -22,6 +23,8 @@ public class UnitDiplomat : UnitPlayer
     int PassPercent { get { return _PassPercent + mBaseObj.BuffProp.Percentage; } }
 
     private MotionActionSingle mMotionAttack = null;
+
+    int PassPercentTest { get { return 10 + mBaseObj.SpecProp.Level * 5; } }
     
     void Start()
     {
@@ -31,41 +34,47 @@ public class UnitDiplomat : UnitPlayer
             BasicSpec spec = mBaseObj.SpecProp.GetPrivateFieldValue<BasicSpec>("_Spec");
             spec.damage = 8;
             spec.damagesPerUp[0] = 1;
-            _PassPercent = 10;
+            _PassPercent = PassPercentTest;
+            _ProjectileCount = curLevel;
         }
         else if (curLevel <= 2)
         {
             BasicSpec spec = mBaseObj.SpecProp.GetPrivateFieldValue<BasicSpec>("_Spec");
             spec.damage = 25;
             spec.damagesPerUp[1] = 14;
-            _PassPercent = 20;
+            _PassPercent = PassPercentTest;
+            _ProjectileCount = curLevel;
         }
         else if (curLevel <= 3)
         {
             BasicSpec spec = mBaseObj.SpecProp.GetPrivateFieldValue<BasicSpec>("_Spec");
             spec.damage = 120;
             spec.damagesPerUp[2] = 85;
-            _PassPercent = 30;
+            _PassPercent = PassPercentTest;
+            _ProjectileCount = curLevel;
         }
         else if (curLevel <= 4)
         {
             BasicSpec spec = mBaseObj.SpecProp.GetPrivateFieldValue<BasicSpec>("_Spec");
             spec.damage = 250;
             spec.damagesPerUp[3] = 555;
-            _PassPercent = 40;
+            _PassPercent = PassPercentTest;
+            _ProjectileCount = curLevel;
         }
         else if (curLevel <= 5)
         {
             BasicSpec spec = mBaseObj.SpecProp.GetPrivateFieldValue<BasicSpec>("_Spec");
             spec.damage = 600;
             spec.damagesPerUp[4] = 1220;
-            _PassPercent = 50;
+            _PassPercent = PassPercentTest;
+            _ProjectileCount = curLevel;
         }
         else if (curLevel <= 6)
         {
             BasicSpec spec = mBaseObj.SpecProp.GetPrivateFieldValue<BasicSpec>("_Spec");
             spec.damage = 30000;
-            _PassPercent = 60;
+            _PassPercent = PassPercentTest;
+            _ProjectileCount = curLevel;
         }
 
         mBaseObj.MotionManager.SwitchMotion<MotionAppear>();
@@ -79,7 +88,8 @@ public class UnitDiplomat : UnitPlayer
 
     void OnAttack(int idx)
     {
-        ShootProjectail(mMotionAttack.Target);
+        for(int i = 0; i < _ProjectileCount; ++i)
+            ShootProjectail(mMotionAttack.Target);
     }
     private void ShootProjectail(BaseObject target)
     {
